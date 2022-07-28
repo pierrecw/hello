59addb1789969363382ca0f801cccc8e6843f408
pierrecw <leepierre456@gmail.com>
2022-07-28 16:21:22 +0900

    source added
    source added

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let http = require('http');
+let os = require('os');
+let port = 8080;
+
+http.createServer( (req, res) => {
+  console.log('Requested: ' + req.url);
+  res.writeHead(200, { 'Content-Type':'text/html; charset=utf-8' });
+  res.write('<h1>docker 예제1</h1>');
+  res.write('<h2>Hello node!</h2>');
+  res.end();
+}).listen(port, () => {
+  console.log('Listen ... ' + os.hostname()+':'+port);
+});
